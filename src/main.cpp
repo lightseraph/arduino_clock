344f73b80765e9eeb272826d34abb6b098df1078
light <lightseraph@163.com>
2022-12-14 19:17:12 +0800

    12.14.2

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,34 @@
 #include <Arduino.h>
+#include <DS3231.h>
 
-void setup() {
-  // put your setup code here, to run once:
+#define ONBOARD_LED 13
+#define KEY_A 2
+#define KEY_B 3
+
+RTClib myRTC;
+
+void togglePin(uint8_t pin)
+{
+  digitalWrite(pin, !digitalRead(pin));
 }
 
-void loop() {
+void onKey_APress()
+{
+}
+
+void setup()
+{
+  // put your setup code here, to run once:
+  pinMode(ONBOARD_LED, OUTPUT);
+  pinMode(KEY_A, INPUT_PULLUP);
+  pinMode(KEY_B, INPUT_PULLUP);
+  Serial.begin(115200);
+  attachInterrupt(digitalPinToInterrupt(KEY_A), onKey_APress, LOW);
+}
+
+void loop()
+{
   // put your main code here, to run repeatedly:
+  togglePin(ONBOARD_LED);
+  delay(500);
 }
