22376eba3443d658b39ad7984060fef889980dc0
jerry <lightseraph@163.com>
2022-12-18 22:56:22 +0800

    12.18.1

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 #include <DS3231.h>
+#include "MsTimer2.h"
 
 #define ONBOARD_LED 13
 #define KEY_A 2
@@ -8,7 +9,9 @@
 
 RTClib myRTC;
 uint8_t digi_seg[7] = {2, 3, 4, 5, 6, 7, 8};
-int count = 0;
+uint8_t digits[2] = {9, 10};
+int tenDigit = 0;
+int singleDigit = 0;
 
 int num[10][7] = {
     {0, 0, 0, 0, 0, 0, 1}, // 数字0
@@ -28,13 +31,30 @@ void togglePin(uint8_t pin)
   digitalWrite(pin, !digitalRead(pin));
 }
 
-int calc(int a, int b, int c)
+void displayNum(uint8_t number, uint8_t digit)
 {
-  return a + b - c;
+  for (int i = 0; i < 7; i++)
+  {
+    digitalWrite(digi_seg[i], num[number][i]);
+  }
+  for (int i = 0; i < 2; i++)
+  {
+    digitalWrite(digits[i], LOW);
+  }
+  digitalWrite(digits[digit], HIGH);
+  delay(10);
 }
 
-void onKey_APress()
+void count()
 {
+  singleDigit++;
+  if (singleDigit > 9)
+  {
+    singleDigit = 0;
+    tenDigit++;
+  }
+  if (tenDigit > 9)
+    tenDigit = 0;
 }
 
 void setup()
@@ -46,13 +66,17 @@ void setup()
     pinMode(digi_seg[i], OUTPUT);
     digitalWrite(digi_seg[i], HIGH);
   }
+
+  for (int i = 0; i < 2; i++)
+  {
+    pinMode(digits[i], OUTPUT);
+    digitalWrite(digits[i], LOW);
+  }
+  MsTimer2::set(1000, count);
+  MsTimer2::start();
   Serial.begin(115200);
   // attachInterrupt(digitalPinToInterrupt(KEY_A), onKey_APress, LOW);
   /* delay(3000);
-  int a = 89;
-  int b = 18;
-  int c = 21;
-  int sum = calc(a, b, c);
   Serial.println(sum); */
 }
 
@@ -60,18 +84,7 @@ void loop()
 {
   // put your main code here, to run repeatedly:
 
-  /* for (int i = 0; i < 7; i++)
-  {
-    togglePin(digi_seg[i]);
-    delay(500);
-  } */
+  displayNum(singleDigit, 1);
 
-  for (int j = 0; j < 10; j++)
-  {
-    for (int i = 0; i < 7; i++)
-    {
-      digitalWrite(digi_seg[i], num[j][i]);
-    }
-    delay(1000);
-  }
+  displayNum(tenDigit, 0);
 }
